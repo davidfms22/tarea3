36f230648766366873bd5c6aa45e96264c2da8b3
DavidMtz <df.martinez1@uniandes.edu.co>
2016-03-01 23:14:24 -0500

    algo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,9 +15,9 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/", (req, res) -> "AYUDEME ANDRES NO SEA ASI DE RABON");
+//    get("/", (req, res) -> "AYUDEME ANDRES NO SEA ASI DE RABON");
 
-    get("/hello", (request, response) -> {
+    get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
