0342606eef5279ea94bc0b44ba37ef928951d93d
DavidMtz <df.martinez1@uniandes.edu.co>
2016-03-02 20:51:19 -0500

    listos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import dm.uniandes.app.FileFinder;
+import dm.uniandes.app.Statistics;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
@@ -16,17 +18,36 @@ public class Main {
 	private static String filePath1 = "///C:/GT_Workspace/ECOS/Tarea/program1/Program1/target/test1.txt";
 
 	public static void main(String[] args) {
+		
+		HashMap<String, List<Double>> data1 = new HashMap<String, List<Double>>();
+		try {
+			data1 = FileFinder.getPairData(filePath1);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HashMap<String, Double> result1 = new HashMap<String, Double>();
+		result1=Statistics.linearRegression(data1.get("x"), data1.get("y"));
+		
 
 		port(Integer.valueOf(System.getenv("PORT")));
 		staticFileLocation("/public");
+		Double a1 = result1.get("beta0");
+		Double a2 = result1.get("beta1");
+		Double a3 = result1.get("rXY");
+		Double a4 = result1.get("r2");
+		Double a5 = result1.get("yK");
 
 		get("/", (request, response) -> {
 			Map<String, Object> attributes = new HashMap<>();
-			attributes.put("A1", "beta0");
-			attributes.put("A2", "beta1");
-			attributes.put("A3", "rXY");
-			attributes.put("A4", "R2");
-			attributes.put("A5", "Yk");
+			attributes.put("A1", a1);
+			attributes.put("A2", a2);
+			attributes.put("A3", a3);
+			attributes.put("A4", a4);
+			attributes.put("A5", a5);
 			attributes.put("C1", "beta0");
 			attributes.put("C2", "beta1");
 			attributes.put("C3", "rXY");
