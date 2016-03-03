9e21b2553a971eff6a7d9962f406a2b4c1f7c47d
DavidMtz <df.martinez1@uniandes.edu.co>
2016-03-02 19:55:14 -0500

    integrando

---
+++ src/main/java/FileFinder.java
@@ -0,0 +1,55 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public final class FileFinder {
+
+	public static LinkedList<Double> getData(String filePath) throws FileNotFoundException, IOException {
+		String num;
+		FileReader file = new FileReader(filePath);
+		BufferedReader linesFile = new BufferedReader(file);
+		LinkedList<Double> data = new LinkedList<Double>();
+
+		while ((num = linesFile.readLine()) != null) {
+			if (!num.equals("")) {
+				data.add(Double.parseDouble(num));
+			}
+
+		}
+		linesFile.close();
+		return data;
+	}
+	
+	public static HashMap<String, List<Double>> getPairData(String filePath) throws FileNotFoundException, IOException {
+		
+		FileReader file = new FileReader(filePath);
+		BufferedReader linesFile = new BufferedReader(file);
+		String line;
+		List<Double> x = new ArrayList<Double>();
+		List<Double> y = new ArrayList<Double>();
+
+		while ((line = linesFile.readLine()) != null) {
+			if (!line.equals("")) {
+				String nums[]=line.split("-");
+				x.add(Double.parseDouble(nums[0]));
+				y.add(Double.parseDouble(nums[1]));
+			}
+
+		}
+		linesFile.close();
+        
+        HashMap<String, List<Double>> pairData = new HashMap<String, List<Double>>();
+        pairData.put("x", x);
+        pairData.put("y", y);
+        
+        return pairData;
+	}
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import static spark.Spark.get;
 import static spark.SparkBase.port;
 import static spark.SparkBase.staticFileLocation;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import spark.ModelAndView;
@@ -10,20 +13,37 @@ import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
 
-  public static void main(String[] args) {
+	private static String filePath1 = "///C:/GT_Workspace/ECOS/Tarea/program1/Program1/target/test1.txt";
 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+	public static void main(String[] args) {
+		
+		HashMap<String, List<Double>> data1 = new HashMap<String, List<Double>>();
+		try {
+			data1 = FileFinder.getPairData(filePath1);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HashMap<String, Double> result1 = new HashMap<String, Double>();
+		result1=Statistics.linearRegression(data1.get("x"), data1.get("y"));
 
-//    get("/", (req, res) -> "AYUDEME ANDRES NO SEA ASI DE RABON");
+		port(Integer.valueOf(System.getenv("PORT")));
+		staticFileLocation("/public");
 
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
+		get("/", (request, response) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put("A1", "22");
+			attributes.put("A2", "22");
+			attributes.put("A3", "22");
+			attributes.put("A4", "22");
+			attributes.put("A5", "22");
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+			return new ModelAndView(attributes, "index.ftl");
+		} , new FreeMarkerEngine());
 
-  }
+	}
 
 }
